cc51cc4f39205d58f5756f4c07217b828bacfc91
tonimota <toni.elton1993@gmail.com>
2018-01-21 22:04:56 -0200

    [FEAT] Include test js

---
+++ fundamentos/arrays.js
@@ -0,0 +1,11 @@
+const valores = [1, 2, 3, 4, 5]
+console.log(valores)
+
+console.log(valores[0],valores[4])
+
+valores.push(6,7,8,9)
+console.log(valores)
+valores.pop();
+console.log(valores)
+delete valores[0]
+console.log(valores)
